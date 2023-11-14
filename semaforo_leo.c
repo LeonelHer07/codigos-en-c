d6ba36a5ea9f75c8b547f5f26b002a45ab8654c3
LeonelHer07 <150768768+LeonelHer07@users.noreply.github.com>
2023-11-13 22:57:41 -0600

    Add files via upload

---
+++ semaforo_leo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	while(1){
+		system("cls");
+		printf("LUZ VERDE");
+		system("Color A0");
+		sleep(20);
+		system("cls");
+		printf("LUZ AMARILLA");
+		system("Color E1");
+		sleep(5);
+		system("cls");
+		printf("LUZ ROJA");
+		system("Color 48");
+		sleep(15);
+	}
+	
+		
+	return 0;
+}
+
